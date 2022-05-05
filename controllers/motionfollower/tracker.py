7df7586e3b56fe47b7c582b74f8f12085ce706d9
joubiri <achraf.joubiti@usmba.ac.ma>
2022-05-05 06:45:23 +0000

    initial

---
+++ controllers/motionfollower/tracker.py
@@ -0,0 +1,65 @@
+import cv2
+import numpy as np
+from controller import Robot,Camera
+
+
+class imagetransform:
+    @staticmethod
+    def convert(image):
+        result=np.asarray(image, dtype=np.uint8)
+        result= cv2.cvtColor(result,cv2.COLOR_BGRA2RGB)
+        result= cv2.rotate(result,cv2.ROTATE_90_CLOCKWISE)
+        return cv2.flip(result,1)
+
+class image(Camera):
+    def __init__(self,timestep):
+        super().__init__('camera')
+        self.enable(timestep)
+
+    def convert(self):
+        return imagetransform.convert(self.getImageArray())
+robot = Robot()
+timestep = int(robot.getBasicTimeStep())
+
+camera = image(timestep)
+
+motor_left = robot.getDevice('left wheel motor')
+motor_right = robot.getDevice('right wheel motor')
+motor_left.setPosition(float('inf'))
+motor_right.setPosition(float('inf'))
+motor_left.setVelocity(0)
+motor_right.setVelocity(0)
+
+def get_image_from_camera():
+
+    img = camera.getImageArray()
+    img = np.asarray(img, dtype=np.uint8)
+    img = cv2.cvtColor(img, cv2.COLOR_BGRA2RGB)
+    img = cv2.rotate(img, cv2.ROTATE_90_CLOCKWISE)
+    return cv2.flip(img, 1)
+
+
+# Main control loop
+while robot.step(timestep) != -1:
+    img = camera.convert()
+    img=cv2.cvtColor(img,cv2.COLOR_RGB2HSV)
+    ll=np.array([25,41,0])
+    ul=np.array([173,255,255])
+    mask=cv2.inRange(img,ll,ul)
+    
+    (cnts,hierarchy)=cv2.findContours(mask.astype(np.uint8).copy(),cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)[-2:]
+    contour=max(cnts, key=cv2.contourArea)
+    M=cv2.moments(contour)
+    x=int(M["m10"] / M["m00"])
+    
+    distance=camera.getWidth()/2 - x
+    print(x)
+    #print(camera.getWidth())
+    
+    motor_left.setVelocity(-distance*0.1)
+    motor_right.setVelocity(distance*0.1)
+    
+
+
+   
+    
